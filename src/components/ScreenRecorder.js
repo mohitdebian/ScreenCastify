5a68305b3bcfa2bfa6296758b7c02c69dc6c1492
Mohit Singh <mohitdebian@gmail.com>
2024-11-25 23:09:58 +0530

    UI Update

--- src/components/ScreenRecorder.js
+++ src/components/ScreenRecorder.js
@@ -97,7 +97,7 @@ const ScreenRecorder = () => {
   return (
     <div className="p-4 bg-black rounded-lg shadow-lg">
       {!stream && !videoUrl ? (
-        <video src="/Final.mp4" autoPlay alt="Screen preview" className="rounded-lg mb-4" />
+        <video src="/Final.mp4" controls autoPlay alt="Screen preview" className="rounded-lg mb-4" />
       ) : (
         <video
           ref={videoRef}
