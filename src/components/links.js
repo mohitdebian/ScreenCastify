5aea5d32e8e3a00d04005b3206537b31f56162c3
Mohit Singh <mohitdebian@gmail.com>
2024-11-26 12:41:02 +0530

    Updated links

--- src/components/links.js
+++ src/components/links.js
@@ -2,7 +2,7 @@
 
 const links = {
     twitter: 'https://x.com/mohitdebian', // Replace with your Twitter link
-    github: 'https://github.com/mohitdebian', // Replace with your GitHub link
+    github: 'https://github.com/mohitdebian/ScreenCastify-Web', // Replace with your GitHub link
     productHunt: 'https://www.producthunt.com/posts/screencastify-2?embed=true&utm_souce=badge-screencastify-2',
     // Add more links as needed
   };
